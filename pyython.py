3f8b0157109b524a7761a0fc647fdd5ed1f6a050
swamin07 <swaminkhond07@gmail.com>
2025-04-05 18:50:46 +0530

    Create pyython.py

---
+++ pyython.py
@@ -0,0 +1,9 @@
+# Initialize the sum to 0
+total_sum = 0
+
+# Loop through numbers from 1 to 50
+for number in range(1, 51):
+    total_sum += number  # Add each number to the total sum
+
+# Display the final sum
+print("The sum of integers from 1 to 50 is:", total_sum)
